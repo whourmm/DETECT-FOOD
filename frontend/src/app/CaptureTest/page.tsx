4f496520b68820921d0944f3af14e9858e46cd41
NaMMly YuMMy <andavarinthorn1409@gmail.com>
2024-08-14 22:28:59 +0700

    finish get base64 image

--- frontend/src/app/CaptureTest/page.tsx
+++ frontend/src/app/CaptureTest/page.tsx
@@ -1,62 +1,53 @@
 "use client";
+import React, { useRef, useState, useEffect } from "react";
+import Image from "next/image";
 import axios from "axios";
-import React, { useRef, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-const CameraCapture: React.FC = () => {
+const CaptureTest: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [videoIsShow, setVideoIsShow] = useState<Boolean>(false);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [capturedImage, setCapturedImage] = useState<string | null>(null);
   const [cameraActive, setCameraActive] = useState(false);
 
+  useEffect(() => {
+    startCamera();
+    return () => {
+      stopCamera();
+    };
+  }, []);
+
   const startCamera = async () => {
+    setCameraActive(true);
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
       if (videoRef.current) {
-        setVideoIsShow(true);
         videoRef.current.srcObject = stream;
-        videoRef.current.play();
+        videoRef.current.onloadedmetadata = () => {
+          videoRef.current?.play().catch((error) => {
+            console.error("Error playing the video", error);
+          });
+        };
+        videoRef.current.onplay = () => {
+          console.log("Video is playing");
+        };
+        setCapturedImage(null);
       }
     } catch (error) {
       console.error("Error accessing the camera", error);
     }
   };
-
-  const capturePhoto = () => {
-    if (videoRef.current && canvasRef.current) {
-      const context = canvasRef.current.getContext("2d");
-      canvasRef.current.width = videoRef.current.videoWidth;
-      canvasRef.current.height = videoRef.current.videoHeight;
-      context?.drawImage(
-        videoRef.current,
-        0,
-        0,
-        canvasRef.current.width,
-        canvasRef.current.height
-      );
-      setCapturedImage(canvasRef.current.toDataURL("image/png"));
-      stopCamera();
-    }
-  };
-  const stopCamera = () => {
-    if (videoRef.current && videoRef.current.srcObject) {
-      const stream = videoRef.current.srcObject as MediaStream; // Type assertion
-      const tracks = stream.getTracks();
-      tracks.forEach((track) => track.stop());
-      videoRef.current.srcObject = null;
-    }
-    setCameraActive(false);
-  };
-
   const sendToBackend = async () => {
     if (capturedImage) {
       try {
         const response = await axios.post(
-          "http://your-backend-api.com/upload",
+          "http://localhost:8000/upload", // Your Python backend URL
           {
             image: capturedImage,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
         );
         console.log("Image uploaded successfully", response.data);
@@ -65,51 +56,115 @@ const CameraCapture: React.FC = () => {
       }
     }
   };
+  const capturePhoto = () => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+
+      if (!context) {
+        console.error("Failed to get canvas context.");
+        return;
+      }
+
+      canvasRef.current.width = videoRef.current.videoWidth;
+      canvasRef.current.height = videoRef.current.videoHeight;
+
+      // Check video dimensions
+      console.log(
+        "Video dimensions:",
+        videoRef.current.videoWidth,
+        videoRef.current.videoHeight
+      );
+
+      // Draw the video frame onto the canvas
+      context.drawImage(
+        videoRef.current,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+
+      // Generate data URL
+      const image = canvasRef.current.toDataURL("image/png");
+
+      // Check data URL length
+      console.log("Data URL length:", image.length);
+      console.log("Data URL:", image);
+
+      if (image.length > 0) {
+        setCapturedImage(image);
+      } else {
+        console.error("Failed to generate image data URL.");
+      }
+    } else {
+      console.error("Video or canvas reference is null.");
+    }
+  };
+
+  const stopCamera = () => {
+    capturePhoto();
+    if (videoRef.current && videoRef.current.srcObject) {
+      const stream = videoRef.current.srcObject as MediaStream;
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      videoRef.current.srcObject = null;
+    }
+
+    setCameraActive(false);
+  };
 
   return (
-    <div className="">
-      <div className="p-5 text-white bg-blue-500 h-[10vh] font-bold text-2xl flex items-center justify-start">
-        <FontAwesomeIcon icon={faArrowLeft} className="mr-10" />
-        CAPTURE TEST
+    <div>
+      <div className="relative flex justify-center ">
+        {capturedImage && !cameraActive && (
+          <div className="relative  w-[100vw] h-[100vh] flex flex-col items-center">
+            <img
+              src={capturedImage}
+              alt="Captured"
+              className="w-full h-full object-cover"
+            />
+            <button
+              className="fixed bottom-[20vh] bg-white px-8 py-3 font-bold rounded-[5vh]"
+              onClick={sendToBackend}
+            >
+              {" "}
+              See your result
+            </button>
+          </div>
+        )}
+        {cameraActive && (
+          <div className="relative w-[100vw] h-[100vh] flex flex-col items-center">
+            <video
+              ref={videoRef}
+              className="w-full h-full object-cover"
+              autoPlay
+              playsInline
+            ></video>
+          </div>
+        )}
       </div>
-      <div className="relative flex justify-center mt-[10vh]">
-        <div className="relative z-50 w-[80vw] h-[50vh] border-[0.8vh] border-green-500 p-4 rounded-[1vh]">
-          {/* Ensure the content div has relative positioning */}
-        </div>
-        {/* <div className="overlay-outside"></div> */}
-        <video
-          ref={videoRef}
-          width="300"
-          height="300"
-          className="absolute w-full h-full z-10 object-cover"
-        ></video>
+
+      <div className="text-white bg-[#B7E3FE] h-[10vh] font-bold w-full fixed bottom-0 rounded-t-[5vh] px-10 py-10 flex justify-center items-center">
+        <Image
+          src={"/img/Camera.svg"}
+          alt="cover"
+          width={80}
+          height={80}
+          className="z-50 mb-5"
+          onClick={cameraActive ? stopCamera : startCamera}
+        />
+
+        <div className="z-10 fixed bg-[#B7E3FE] w-[25vw] h-[25vw] p-5 bottom-[1vh] left-1/2 transform -translate-x-1/2 rounded-[10vh]"></div>
       </div>
-      <h1 className="z-50 relative flex justify-center bg-white mt-10 font-bold text-[2vh]">
-        See your results
-      </h1>
-      <div className="z-50 relative flex justify-center space-x-4 mt-4">
-        <button
-          onClick={startCamera}
-          className="bg-blue-500 text-white p-2 rounded-md"
-        >
-          Start Camera
-        </button>
-        <button onClick={capturePhoto}>Capture Photo</button>
-      </div>
+
       <canvas
         ref={canvasRef}
         width="300"
         height="300"
         style={{ display: "none" }}
       ></canvas>
-      {capturedImage && (
-        <div>
-          <img src={capturedImage} alt="Captured" />
-          <button onClick={sendToBackend}>Upload to Backend</button>
-        </div>
-      )}
     </div>
   );
 };
 
-export default CameraCapture;
+export default CaptureTest;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const goToCapturePage = () => {
+    router.push("/CaptureTest");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <title>Title</title>
+      <div className="">
+        <button
+          className="bg-blue-200 p-5 rounded-[10vh] font-bold"
+          onClick={goToCapturePage}
+        >
+          Capture Image Feature
+        </button>
+      </div>
     </main>
   );
 }
